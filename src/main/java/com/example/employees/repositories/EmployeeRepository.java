d472b6686a3d576c7b887bc567ad932db944aee7
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-02-25 14:49:13 +0000

    Added Employee class and controllers

--- src/main/java/com/example/employees/repositories/EmployeeRepository.java
+++ src/main/java/com/example/employees/repositories/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package com.example.employees.repositories;
 
-public interface EmployeeRepository {
+import com.example.employees.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
--- src/test/java/com/example/employees/EmployeesApplicationTests.java
+++ src/test/java/com/example/employees/EmployeesApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.employees;
 
+import com.example.employees.models.Employee;
+import com.example.employees.repositories.EmployeeRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EmployeesApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee(){
+		Employee gerry = new Employee("Gerry", 45, "ger3456", "gwaterston@yaboo.com");
+		employeeRepository.save(gerry);
 	}
 
 }
